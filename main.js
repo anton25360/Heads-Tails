6e553c210aa4f19bb8dd8ea8cec81b5293e61704
Anton Webb <anton25360@gmail.com>
2019-11-26 16:27:29 +0000

    gradient implemented

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function outputValue(result){
     console.log(result);
+
+    if (result == 'Heads') {
+        document.querySelector('.target').innerHTML = result
+        document.querySelector('.target').style = 'background: linear-gradient(to right, #43cea2, #185a9d); -webkit-background-clip:text; -webkit-text-fill-color:transparent;'
+    } else {
+        document.querySelector('.target').innerHTML = result
+        document.querySelector('.target').style = 'background: linear-gradient(to right, #ee0979, #ff6a00); -webkit-background-clip:text; -webkit-text-fill-color:transparent;'
+    }
     
 }
 
@@ -7,11 +15,14 @@ function coinFlip(){
     let result =  Math.floor(Math.random() * 2);
     
     if (result === 1) {
-        outputValue('heads')
+        outputValue('Heads')
     } else {
-        outputValue('tails')
+        outputValue('Tails')
     }
 }
 
 coinFlip()
 
+document.getElementById('body').addEventListener('click', function(){
+    location.reload(); 
+})
